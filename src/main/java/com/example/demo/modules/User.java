3b0ed8aa08ce27162d9f64e495b61c9dd9f027c2
cherjoe <cherylrjoseph0@gmail.com>
2020-05-29 13:19:26 +0530

    initial commit

---
+++ src/main/java/com/example/demo/modules/User.java
@@ -0,0 +1,50 @@
+package com.example.demo.modules;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User 
+{
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String email;
+	private String name;
+	
+	private User() {}
+
+	public User(String email, String name) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
